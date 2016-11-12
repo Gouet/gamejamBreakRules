d2b55278744a6a918f9f8b2272f182fbe2bf5294
XenoDaimon <theo.touzet@epitech.eu>
2016-11-12 20:58:03 +0100

    Player movesgit checkout GameJamBreakRules/ProjectSettings/!

--- GameJamBreakRules/Assets/scripts/Player/PlayerAnimation.cs
+++ GameJamBreakRules/Assets/scripts/Player/PlayerAnimation.cs
@@ -2,27 +2,57 @@
 using System.Collections;
 
 [RequireComponent (typeof (Animator))]
+[RequireComponent (typeof (Rigidbody2D))]
 public class PlayerAnimation : MonoBehaviour {
 
 	private Animator	anim;
+	private Rigidbody2D rigid;
+	private Vector3 force;
+	private bool jumping = false;
+
+	public float forceJump = 2;
+	public float forceRun = 0.5f;
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log ("Test");
+		if (coll.gameObject.tag == "DeathObject" && !anim.GetBool("IsDead")) {
+			anim.SetBool("IsDead", true);
+			anim.SetTrigger ("Dies");
+		}
+	}
 
 	void Start () {
 		anim = this.GetComponent<Animator> ();
+		rigid = this.GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
-		if (Input.GetButton ("Jump"))
-			anim.SetTrigger ("Jump");
-		else
-			anim.ResetTrigger ("Jump");
-		if (Input.GetButton ("Horizontal")) {
-			Vector3 vec = this.transform.localScale;
-			if ((Input.GetAxis ("Horizontal") > 0 && vec.x > 0) || (Input.GetAxis("Horizontal") < 0 && vec.x < 0))
-				vec.x *= -1;
-		this.transform.localScale = vec;
-		anim.SetBool ("IsRunning", true);
+		if (!anim.GetBool ("IsDead")) {
+			if (Input.GetButton ("Jump") && !jumping) {
+				anim.SetTrigger ("Jump");
+				force = (transform.up * forceJump) / Time.fixedDeltaTime;
+				rigid.AddForce (force);
+				jumping = true;
+			}
+			else
+				anim.ResetTrigger ("Jump");
+
+			if (Input.GetButton ("Horizontal")) {
+				Vector3 vec = this.transform.localScale;
+				if ((Input.GetAxis ("Horizontal") > 0 && vec.x > 0) || (Input.GetAxis ("Horizontal") < 0 && vec.x < 0))
+					vec.x *= -1;
+				this.transform.localScale = vec;
+				anim.SetBool ("IsRunning", true);
+				force = (transform.right * forceRun * vec.x * -1) / Time.deltaTime;
+				rigid.AddForce (force);
+			} else
+				anim.SetBool ("IsRunning", false);
+
+			if (jumping && rigid.velocity.y == 0)
+				jumping = false;
 		}
-		else
-			anim.SetBool ("IsRunning", false);
+	}
+
+	void FixedUpdate() {
 	}
 }
